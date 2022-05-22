de8f4ecbfc3222d82df25754a43c2f69cbbca0d9
aleco <sasha.sad@mail.ru>
2022-05-22 23:28:30 +0300

    22/05/22 23.28

--- src/Study/HashSetEx.java
+++ src/Study/HashSetEx.java
@@ -1,7 +1,43 @@
 package Study;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class HashSetEx {
     public static void main(String[] args) {
+
+//        int[] a = {3,5,1};
+//        System.out.println(a[1]);
+
+        HashSet<String> h = new HashSet<>();
+        h.add("lemur");
+        h.add("orangatang");
+        h.add("spider monkey");
+        h.add("silverback gorilla");
+
+        h.remove("orangatang");
+
+        System.out.println(h);
+        System.out.println(h.size());
+        System.out.println(h.contains("spider monkey"));
+        System.out.println(h.isEmpty());
+
+        HashSet<Integer> hashBrowns = new HashSet();
+        hashBrowns.add(13);
+        hashBrowns.add(24);
+        hashBrowns.add(5);
+
+        //hashBrowns.clear();
+        Object[] has = hashBrowns.toArray();
+        System.out.println(has[0]);
+
+        System.out.println(hashBrowns.hashCode());
+
+        Iterator<Integer> it = hashBrowns.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+
 
     }
 }
---
+++ src/Study/MathLibraryEx.java
@@ -0,0 +1,27 @@
+package Study;
+
+public class MathLibraryEx {
+    public static void main(String[] args) {
+        System.out.println(Math.E);
+        System.out.println(Math.PI);
+        System.out.println(Math.abs(-12));
+        System.out.println(Math.acos(1));
+        System.out.println(Math.atan(45));
+        System.out.println(Math.cosh(-0.2));
+        System.out.println(Math.exp(2));
+        System.out.println(Math.floor(6.4564654));
+        System.out.println(Math.floorDiv(9,6));
+        System.out.println(Math.getExponent(64));
+        System.out.println(Math.hypot(3,4));
+        System.out.println(Math.incrementExact(8));
+        System.out.println(Math.log(Math.E));
+        System.out.println(Math.max(5,8));
+        System.out.println(Math.negateExact(8));
+        System.out.println(Math.nextAfter(31,42));
+        System.out.println(Math.pow(3,3));
+        System.out.println(Math.scalb(3,2));
+        System.out.println(Math.random());
+
+
+    }
+}
